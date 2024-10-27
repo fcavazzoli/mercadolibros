6c007d5ba891dcae661bf63907b96bee6694ebb3
Federico Cavazzoli <fedecavazzoli@gmail.com>
2024-10-27 15:20:26 -0300

    feat: add cors to backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 import router from "./src/routes/index.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "*" }));
 
 app.use("/api", router);
 
