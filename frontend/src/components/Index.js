be77f567ffe9b1dbaa5717c88df9df25c679faa6
TaniaFrieden <tfriedenberger@fi.uba.ar>
2024-11-20 18:02:59 -0300

    arreglo home

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -1,43 +1,34 @@
-import { useState, useEffect } from "react"
-import logo from '../css/logo.svg';
+import { useState, useEffect } from "react";
+import logo from '../css/logo.svg'; // Asegúrate de usar el logo correcto
 import '../css/App.css';
-import * as server from '../helpers/HttpProtocol'
-import Header from './Header'
+import * as server from '../helpers/HttpProtocol';
+import Header from './Header';
 
 function Render() {
   const [name, setName] = useState("...");
 
-   useEffect(() => { identifyMe(setName); }, []);
-  
-  return (<Header>
-    <div className="App max-height-possible">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org"
-          target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+  useEffect(() => { identifyMe(setName); }, []);
 
-        <p>Hello { name }</p>
-
-        <button >Libro nuevo</button>
-      </header>
-    </div>
-  </Header>);
+  return (
+    <Header>
+      <div className="App max-height-possible">
+        <header className="App-header">
+          <img src={"/logo192.png"} className="App-logo-centered" alt="logo" />
+          <h1>¡Bienvenido a Mercado Libros!</h1>
+          <p>¡Explora, agrega y gestiona tus libros favoritos!</p>
+        </header>
+      </div>
+    </Header>
+  );
 }
 
 async function identifyMe(setName) {
   try {
     const data = await server.get("users/me", {});
-    
-    setName(data.user?.name ?? "Guest");
-
+    setName(data.user?.name ?? "Invitado");
   } catch (error) {
     console.error("Error fetching user:", error);
   }
-};
+}
 
 export default Render;
