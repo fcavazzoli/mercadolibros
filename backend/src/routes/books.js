dff1c408658a4d494fbd4634da0bd3b518a538ee
Federico Cavazzoli <fedecavazzoli@gmail.com>
2024-11-24 10:52:08 -0300

    fix: route

--- backend/src/routes/books.js
+++ backend/src/routes/books.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import { authenticateUser } from "../middleware/auth.js";
-import { create, getAll, getBook, assingBook, updateBook } from "../controllers/booksController.js";
+import { create, getAll, getBook, assingBook, updateBook, getBooksBySession } from "../controllers/booksController.js";
 
 const router = express.Router();
 
 router.get("/", authenticateUser, getAll)
 router.get("/:id", authenticateUser, getBook)
+router.get('/myBooks', authenticateUser, getBooksBySession)
+
 
 router.patch('/:id', authenticateUser, updateBook)
 
