6df502a11e1ad53cb5dc110e5f9e4e7746583c3d
Federico Cavazzoli <federicocavazzoli@Federicos-MacBook-Pro-2.local>
2024-12-03 19:19:34 -0300

    feat: add safe request middleware

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
+import 'express-async-errors';
 import express from "express";
 import cors from "cors";
 
 import router from "./src/routes/index.js";
+import { useSafeRequest } from "./src/middleware/safe.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors({ origin: "*" }));
-
 app.use("/api", router);
 app.use("/images", express.static("../images"));
+app.use(useSafeRequest)
 
 app.listen(3000, () => {
     console.log("Starting server...");
