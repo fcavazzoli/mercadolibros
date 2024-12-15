fe687213a57b908f8f544106d1517043e0e88a5e
Nicolás <58600338+NicolasZanardo@users.noreply.github.com>
2024-12-15 14:33:19 -0300

    Acciones sobre libros propios (#43)

--- frontend/src/components/libros/LibroList.js
+++ frontend/src/components/libros/LibroList.js
@@ -10,6 +10,10 @@ const LibroList = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
 
+    const getEventBook = function(event) {
+        return filteredBooks[parseInt(event.currentTarget.getAttribute("index"))];
+    }
+
     useEffect(() => {
         const fetchBooks = async () => {
             try {
@@ -22,10 +26,18 @@ const LibroList = () => {
         fetchBooks();
     }, []);
 
-    const handleDelete = async (bookId) => {
+    const handleDelete = async (evClick) => {
+        const book = getEventBook(evClick);
+
         if (window.confirm('¿Estás seguro de que deseas eliminar este libro?')) {
             try {
-                let response = await deleteBook(bookId);
+                let response = await deleteBook(book.id);
+                
+                if (response.message.includes("ExchangeProposal")) {
+                    alert('No puede eliminar el libro ya que se encuenta en un trueque no cerrado');
+                    return;
+                }
+
                 setBooks(await getMyBooks() || []);
                 alert(response.message);
             } catch (error) {
@@ -35,8 +47,9 @@ const LibroList = () => {
         }
     };
 
-    const handleEdit = (bookId) => {
-        navigate(`/edit-book/${bookId}`);
+    const handleEdit = (evClick) => {
+        const book = getEventBook(evClick);
+        navigate(`/edit-book/${book.id}`);
     };
 
     const filteredBooks = useMemo(() => 
@@ -63,7 +76,15 @@ const LibroList = () => {
               className="search-bar"
             />
           </div>
-          <BooksGrid books={filteredBooks}></BooksGrid>
+          <BooksGrid books={filteredBooks}>
+             {/* Child buttons will receive the 'book' prop */}
+             <button className='libro-update' onClick={(e) => handleEdit(e)}>
+                Update
+            </button>
+            <button className='libro-delete' onClick={(e) => handleDelete(e)}>
+                Delete
+            </button>
+          </BooksGrid>
 
           
         </div>
